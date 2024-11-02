50a0c2fe0b394ee734e8a6af64b73381128ccbf4
SRCS <3879937469@qq.com>
2024-11-03 00:02:42 +0800

    Initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private CharacterController character;
+
+    [SerializeField]
+    private float gravity;
+    [SerializeField]
+    private float jumpForce;
+
+    private Vector3 direction;
+
+    private void Awake()
+    {
+        gravity *= 2f;
+    }
+
+    private void OnEnable()
+    {
+        direction = Vector3.zero;
+    }
+
+    private void Update()
+    {
+        direction += gravity * Time.deltaTime * Vector3.down;
+
+        if (character.isGrounded)
+        {
+            direction = Vector3.down;
+
+            if (Input.GetButton("Jump"))
+            {
+                direction = Vector3.up * jumpForce;
+            }
+        }
+
+        character.Move(direction * Time.deltaTime);
+    }
+}
